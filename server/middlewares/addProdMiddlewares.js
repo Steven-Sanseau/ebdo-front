7b6547ede6fc40c275acde7f6a17f9ae3f28e0c2
Steven Sanséau <steste.etsets@gmail.com>
2018-01-02 21:54:14 +0100

    add ssl force for production on Heroku

--- server/middlewares/addProdMiddlewares.js
+++ server/middlewares/addProdMiddlewares.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const compression = require('compression')
+const sslRedirect = require('heroku-ssl-redirect')
 
 module.exports = function addProdMiddlewares(app, options) {
   const publicPath = options.publicPath || '/'
@@ -11,6 +12,9 @@ module.exports = function addProdMiddlewares(app, options) {
   // and other good practices on official Express.js docs http://mxs.is/googmy
   app.use(compression())
   app.use(publicPath, express.static(outputPath, { maxage: 86400000 }))
+
+  // Force SSL on production
+  app.use(sslRedirect())
 
   app.get('*', (req, res) => {
     res.set('Cache-Control', 'public, max-age=3600')
