62c3cc88f315016d282335b5d217f0b9d8499bcf
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2018-01-04 12:23:59 +0100

    Fix merge

--- app/stores.js
+++ app/stores.js
@@ -10,11 +10,8 @@ import sagaCheckout from 'saga/checkout'
 import sagaToken from 'saga/token'
 import sagaAddress from 'saga/address'
 import sagaClient from 'saga/client'
-<<<<<<< Updated upstream
 import sagaLogin from 'saga/login'
-=======
 import sagaNewsletter from 'saga/newsletter'
->>>>>>> Stashed changes
 
 class StoreSaga extends React.Component {
   render() {
@@ -31,11 +28,8 @@ const withSagaToken = injectSaga({ key: 'token', saga: sagaToken })
 const withSagaCheckout = injectSaga({ key: 'checklout', saga: sagaCheckout })
 const withSagaClient = injectSaga({ key: 'client', saga: sagaClient })
 const withSagaAddress = injectSaga({ key: 'address', saga: sagaAddress })
-<<<<<<< Updated upstream
 const withSagaLogin = injectSaga({ key: 'login', saga: sagaLogin })
-=======
 const withSagaNewsletter = injectSaga({ key: 'newsletter', saga: sagaNewsletter })
->>>>>>> Stashed changes
 
 export default compose(
   withSagaOffer,
@@ -43,9 +37,6 @@ export default compose(
   withSagaCheckout,
   withSagaAddress,
   withSagaClient,
-<<<<<<< Updated upstream
-  withSagaLogin
-=======
+  withSagaLogin,
   withSagaNewsletter
->>>>>>> Stashed changes
 )(StoreSaga)
