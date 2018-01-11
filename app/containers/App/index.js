65891f90aba2552f67f9c0fbe47bd1205973ad94
Steven Sanséau <steste.etsets@gmail.com>
2018-01-12 00:06:38 +0100

    HAHAHAH

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -23,37 +23,46 @@ export default function App() {
         <meta name="description" content="Abonnement ebdo" />
       </Helmet>
       <Switch>
-        <Redirect from="/accueil" to="/" />
-        <Route exact path="/" component={Home} />
-        <Redirect from="/subscribe" to="/abonnement" />
-        <Route path="/abonnement/:step" component={Checkout} />
-        <Route path="/abonnement" component={Checkout} />
-        <Route exact path="/abo/:type" component={Acknowledgment} />
-        <Redirect from="/offer" to="/offre" />
-        <Route exact path="/offre/:step" component={Offer} />
-        <Route exact path="/offre" component={Offer} />
-        <Redirect from="/try" to="/essai" />
-        <Redirect from="/jessaye" to="/essai" />
-        <Route path="/essai/:step" component={TryIt} />
-        <Route path="/essai" component={TryIt} />
-        <Redirect from="/team" to="/equipe" />
-        <Route exact path="/equipe" component={Team} />
-        <Redirect from="/manifest" to="/manifeste" />
-        <Redirect from="/pourquoi" to="/manifeste" />
-        <Route exact path="/manifeste" component={Manifest} />
-        <Redirect from="/lasource" to="/source" />
-        <Route exact path="/source" component={Source} />
-        <Redirect from="/login" to="/connexion" />
-        <PublicRoute exact path="/connexion" component={Login} />
-        <PublicRoute path="/connexion/:redirect" component={Login} />
-        <Redirect from="/logout" to="/connexion" />
-        <Route exact path="/mentions-legales" component={Mentions} />
-        <Route exact path="/cgv" component={Cgv} />
-        {/* <Route
+        <Route exact path="/v2/" component={Home} />
+
+        <Redirect from="/v2/subscribe" to="/v2/abonnement" />
+        <Route path="/v2/abonnement/:step" component={Checkout} />
+        <Route path="/v2/abonnement" component={Checkout} />
+
+        <Route exact path="/v2/abo/:type" component={Acknowledgment} />
+
+        <Redirect from="/v2/offer" to="/v2/offre" />
+        <Route exact path="/v2/offre/:step" component={Offer} />
+        <Route exact path="/v2/offre" component={Offer} />
+
+        <Redirect from="/v2/try" to="/v2/essai" />
+        <Redirect from="/v2/jessaye" to="/v2/essai" />
+        <Route path="/v2/essai/:step" component={TryIt} />
+        <Route path="/v2/essai" component={TryIt} />
+
+        <Redirect from="/v2/team" to="/v2/equipe" />
+        <Route exact path="/v2/equipe" component={Team} />
+
+        <Redirect from="/v2/manifest" to="/v2/manifeste" />
+        <Redirect from="/v2/pourquoi" to="/v2/manifeste" />
+        <Route exact path="/v2/manifeste" component={Manifest} />
+
+        <Redirect from="/v2/lasource" to="/v2/source" />
+        <Route exact path="/v2/source" component={Source} />
+
+        <Redirect from="/v2/login" to="/v2/connexion" />
+        <PublicRoute exact path="/v2/connexion" component={Login} />
+        <PublicRoute path="/v2/connexion/:redirect" component={Login} />
+
+        <Redirect from="/v2/logout" to="/v2/connexion" />
+
+        <Route exact path="/v2/mentions-legales" component={Mentions} />
+        <Route exact path="/v2/cgv" component={Cgv} />
+        <Route
           component={() => {
             window.location = 'http://ebdo-lejournal.fr'
             return null
-          }} */}
+          }}
         />
       </Switch>
     </div>
