b4c74f379994f7182355871358aa2de57fd34cf4
Steven Sanséau <steste.etsets@gmail.com>
2018-01-15 16:16:08 +0100

    Add aad server tag

--- app/middleware/router.js
+++ app/middleware/router.js
@@ -1,11 +1,42 @@
+import { LOCATION_CHANGE } from 'react-router-redux'
+
 const routerMiddleware = store => next => action => {
-  if (!action.meta || !action.meta.delay) {
+  if (action.type === LOCATION_CHANGE) {
+    if (action.payload.pathname === '/') {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        send_to: 'DC-8312645/site18/hp18+standard'
+      })
+    }
+
+    if (action.payload.pathname === '/source') {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        send_to: 'DC-8312645/site18/source18+standard'
+      })
+    }
+
+    if (action.payload.pathname === '/manifest') {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        send_to: 'DC-8312645/site18/pourquoi+standard'
+      })
+    }
+
+    if (action.payload.pathname === '/equipe') {
+      gtag('event', 'conversion', {
+        allow_custom_scripts: true,
+        send_to: 'DC-8312645/site18/equipe18+standard'
+      })
+    }
     return next(action)
   }
 
-  let timeoutId = setTimeout(() => next(action), action.meta.delay)
-
-  return function cancel() {
-    clearTimeout(timeoutId)
+  if (!action.track) {
+    return next(action)
   }
+
+  return next(action)
 }
+
+export default routerMiddleware
